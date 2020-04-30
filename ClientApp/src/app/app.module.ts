2698657329285234b8751d725cac48c6a06e9856
Arnaud de SARTRE <arnaud.desartre@stago.com>
2020-04-30 11:27:24 +0200

    Ajout d'une boite de dialogue, pour saisir les credentiales

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,42 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
-//import { NavMenuComponent } from './nav-menu/nav-menu.component';
-//import { HomeComponent } from './home/home.component';
-//import { CounterComponent } from './counter/counter.component';
-//import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { ProjectComponent } from './project/project.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { CredentialsComponent } from './credentials/credentials.component';
+import { CreateCredentialComponent, DialogOverviewExampleDialog } from './create-credential/create-credential.component';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MaterialModule } from './material-module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // NavMenuComponent,
-    // HomeComponent,
-    // CounterComponent,
-    // FetchDataComponent,
     MainNavComponent,
     ProjectComponent,
     RepositoriesComponent,
     CredentialsComponent,
+    CreateCredentialComponent,
+    DialogOverviewExampleDialog,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    MatDialogModule,
+    MaterialModule,
     RouterModule.forRoot([
       { path: '', component: ProjectComponent, pathMatch: 'full' },
       { path: 'repositories', component: RepositoriesComponent },
@@ -50,7 +49,10 @@ import { CredentialsComponent } from './credentials/credentials.component';
     MatIconModule,
     MatListModule
   ],
-  providers: [],
+  entryComponents: [
+    CreateCredentialComponent, DialogOverviewExampleDialog
+    ],
+  providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ClientApp/src/app/create-credential/create-credential.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  name: string;
+  key: string;
+}
+
+@Component({
+  selector: 'app-create-credential',
+  templateUrl: './create-credential.component.html',
+  styleUrls: ['./create-credential.component.css']
+})
+export class CreateCredentialComponent implements OnInit {
+
+  name: string;
+  token: string;
+
+  constructor(public dialog: MatDialog) { }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      //width: '250px',
+      //height: '400px',
+      data: { name: this.name, token: this.token}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.token = result;
+    });
+  }
+
+  ngOnInit() {
+  }
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
